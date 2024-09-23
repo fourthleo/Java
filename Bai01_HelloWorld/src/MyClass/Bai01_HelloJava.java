6e98180a72232cd8332dc7066d6eb9d40a6d4f44
fourthleo <truong.bn.cc23cth@ntu.edu.vn>
2024-09-23 20:03:16 +0700

    Bài 01

---
+++ Bai01_HelloWorld/src/MyClass/Bai01_HelloJava.java
@@ -0,0 +1,17 @@
+package MyClass;
+
+public class Bai01_HelloJava 
+{
+
+	public static void main(String[] args) 
+	{
+		int a = 5;
+		int b = 7;
+		int c = a + b;
+		System.out.println("Hôm nay tôi bắt đầu học môn Java");
+		System.out.println("Tổng là: " + c);
+
+
+	}
+
+}
